6883f39caede71a85aef70ad6aaf050836bff270
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2024-04-15 11:44:08 +0200

    tutorial 1 en 2

--- laravel-testing/tests/Feature/ExampleTest.php
+++ laravel-testing/tests/Feature/ExampleTest.php
@@ -16,4 +16,13 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_the_homepage_contains_symfony(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertSee('Symfony');
+
+        $response->assertStatus(200);
+    }
 }
